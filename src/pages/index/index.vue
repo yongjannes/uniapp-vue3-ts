6b88f1ae3d853871256f2abfabc59d5a50b154cc
yongjannes <2380444947@qq.com>
2025-03-07 19:23:59 +0800

    首页轮播图类型定义渲染

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,10 +1,30 @@
 <script setup lang="ts">
-import CustomNavbar from './components/CustomNavbar.vue';
+import type { BannerItem } from '@/types/home.d'
+import {getHomeBannerAPI} from '@/services/home';
+import {ref} from 'vue'
+import { onLoad } from '@dcloudio/uni-app'
+import CustomNavbar from './components/CustomNavbar.vue'
+
+
+const bannerList = ref<BannerItem[]>([])
+
+const getHomeBannerData = async () => {
+  const res = await getHomeBannerAPI()
+  console.log(res)
+  bannerList.value = res.result
+   
+}
+
+onLoad(() => { 
+    getHomeBannerData()
+})
+
+
 </script>
 
 <template>
     <CustomNavbar />
-    <XtxSwiper />
+    <XtxSwiper  :list="bannerList"/>
     
 </template>
 
