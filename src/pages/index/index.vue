7d9fe0a0c5ef34e77e0c813be65972f7addf837c
yongjannes <2380444947@qq.com>
2025-03-08 14:46:59 +0800

    获取首页分类数据

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,21 +1,24 @@
 <script setup lang="ts">
 import type { BannerItem } from '@/types/home.d'
-import {getHomeBannerAPI} from '@/services/home';
+import {getHomeBannerAPI, getHomeCategoryAPI} from '@/services/home';
 import {ref} from 'vue'
 import { onLoad } from '@dcloudio/uni-app'
 import CustomNavbar from './components/CustomNavbar.vue'
 import CategoryPanel from './components/CategoryPanel.vue';
 
 const bannerList = ref<BannerItem[]>([])
-
 const getHomeBannerData = async () => {
   const res = await getHomeBannerAPI()
   bannerList.value = res.result
-   
 }
 
+const getHomeCategoryData = async () => {
+  const res = await getHomeCategoryAPI()
+ 
+}
 onLoad(() => { 
     getHomeBannerData()
+    getHomeCategoryData()
 })
 
 
--- src/services/home.ts
+++ src/services/home.ts
@@ -1,5 +1,5 @@
 import { http } from '@/utils/http'
-import type { BannerItem } from '@/types/home.d.ts'
+import type { BannerItem, CategoryItem } from '@/types/home.d.ts'
 
 /**
  * 首页-广告区域-小程序
@@ -12,5 +12,16 @@ export const getHomeBannerAPI = (distributionSite = 1) => {
     data: {
       distributionSite,
     },
+  })
+}
+
+// services/home.ts
+/**
+ * 首页-前台分类-小程序
+ */
+export const getHomeCategoryAPI = () => {
+  return http<CategoryItem[]>({
+    method: 'GET',
+    url: '/home/category/mutli',
   })
 }
