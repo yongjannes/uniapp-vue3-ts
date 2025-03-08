824320400cb853a10920429c01c3ef31be8db8c6
yongjannes <2380444947@qq.com>
2025-03-08 15:30:42 +0800

    猜你喜欢类型定义和列表渲染

--- src/services/home.ts
+++ src/services/home.ts
@@ -1,5 +1,6 @@
 import { http } from '@/utils/http'
-import type { BannerItem, CategoryItem, HotItem } from '@/types/home.d.ts'
+import type { BannerItem, CategoryItem, GuessItem, HotItem } from '@/types/home.d.ts'
+import type { PageParams, PageResult } from '@/types/global'
 
 /**
  * 首页-广告区域-小程序
@@ -34,5 +35,17 @@ export const getHomeHotAPI = () => {
   return http<HotItem[]>({
     method: 'GET',
     url: '/home/hot/mutli',
+  })
+}
+
+
+/**
+ * 猜你喜欢-小程序
+ */
+export const getHomeGoodsGuessLikeAPI = (data?: PageParams) => {
+  return http<PageResult<GuessItem>>({
+    method: 'GET',
+    url: '/home/goods/guessLike',
+    data,
   })
 }
