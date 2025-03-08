d978d207fa3008e14fc0f822fa41b614fb21a84e
yongjannes <2380444947@qq.com>
2025-03-08 16:56:15 +0800

    热门推荐类型定义

--- src/services/hot.ts
+++ src/services/hot.ts
@@ -1,5 +1,6 @@
 import { http } from '@/utils/http'
 import type { PageParams } from '@/types/global'
+import type { HotResult } from '@/types/hot'
 
 type HotParams = PageParams & {
   /** Tab 项的 id，默认查询全部 Tab 项的第 1 页数据 */
