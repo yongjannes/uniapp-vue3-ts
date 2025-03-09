a773ea2822c0de2027bc71910f135b83234fd3da
yongjannes <2380444947@qq.com>
2025-03-09 15:19:39 +0800

    商品分类一级分类

--- src/pages/category/category.vue
+++ src/pages/category/category.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
+import { getCategoryTopAPI } from '@/services/category'
 import { getHomeBannerAPI } from '@/services/home'
+import type { CategoryTopItem } from '@/types/category'
 import type { BannerItem } from '@/types/home'
 import { onLoad } from '@dcloudio/uni-app'
 import { ref } from 'vue'
@@ -12,8 +14,20 @@ const getBannerData = async () => {
 }
 
 
+// 获取分类列表数据
+const categoryList = ref<CategoryTopItem[]>([])
+const getCategoryTopData = async () => {
+  const res = await getCategoryTopAPI()
+  categoryList.value = res.result
+  console.log(res)
+}
+
+// 高亮下标
+const activeIndex = ref(0)
+
 onLoad(()=>{
   getBannerData()
+  getCategoryTopData()
 })
 </script>
 
@@ -29,8 +43,8 @@ onLoad(()=>{
     <view class="categories">
       <!-- 左侧：一级分类 -->
       <scroll-view class="primary" scroll-y>
-        <view v-for="(item, index) in 10" :key="item" class="item" :class="{ active: index === 0 }">
-          <text class="name"> 居家 </text>
+        <view v-for="(item, index) in categoryList" :key="item.id" class="item" :class="{ active: index === activeIndex }"  @tap="activeIndex = index">
+          <text class="name"> {{ item.name }}</text>
         </view>
       </scroll-view>
       <!-- 右侧：二级分类 -->
