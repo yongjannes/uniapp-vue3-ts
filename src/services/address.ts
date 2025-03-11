ac21662d7af31a55382ebd9a709931f646f5b8a0
yongjannes <2380444947@qq.com>
2025-03-11 16:26:12 +0800

    地址模块：修改地址数据回显

--- src/services/address.ts
+++ src/services/address.ts
@@ -23,3 +23,28 @@ export const getMemberAddressAPI = () => {
     url: '/member/address',
   })
 }
+
+/**
+ * 获取收货地址详情
+ * @param id 地址id(路径参数)
+ */
+export const getMemberAddressByIdAPI = (id: string) => {
+  return http<AddressItem>({
+    method: 'GET',
+    url: `/member/address/${id}`,
+  })
+}
+
+
+/**
+ * 修改收货地址
+ * @param id 地址id(路径参数)
+ * @param data 表单数据(请求体参数)
+ */
+export const putMemberAddressByIdAPI = (id: string, data: AddressParams) => {
+  return http({
+    method: 'PUT',
+    url: `/member/address/${id}`,
+    data,
+  })
+}
