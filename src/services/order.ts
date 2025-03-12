1d69e6336b554c9da4595fd9466d790e5624afea
yongjannes <2380444947@qq.com>
2025-03-12 18:12:23 +0800

    订单列表：列表渲染

--- src/services/order.ts
+++ src/services/order.ts
@@ -1,5 +1,5 @@
 
-import type { OrderCreateParams, OrderLogisticResult, OrderPreResult, OrderResult } from '@/types/order'
+import type { OrderCreateParams, OrderListParams, OrderListResult, OrderLogisticResult, OrderPreResult, OrderResult } from '@/types/order'
 import { http } from '@/utils/http'
 /**
  * 填写订单-获取预付订单
@@ -110,3 +110,16 @@ export const deleteMemberOrderAPI = (data: { ids: string[] }) => {
     data,
   })
 }
+
+/**
+ * 获取订单列表
+ * @param data orderState 订单状态
+ */
+export const getMemberOrderAPI = (data: OrderListParams) => {
+  return http<OrderListResult>({
+    method: 'GET',
+    url: `/member/order`,
+    data,
+  })
+}
+
