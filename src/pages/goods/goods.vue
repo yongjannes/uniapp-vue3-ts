9d2ee0b3062db6272532e1c7be7ef232ab42fab6
yongjannes <2380444947@qq.com>
2025-03-12 14:31:10 +0800

    SKU模块：渲染被选中的值

--- src/pages/goods/goods.vue
+++ src/pages/goods/goods.vue
@@ -2,11 +2,11 @@
 import { getGoodsByIdAPI } from '@/services/goods';
 import type { GoodsResult } from '@/types/goods';
 import { onLoad } from '@dcloudio/uni-app';
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import AddressPanel from './components/AddressPanel.vue'
 import ServicePanel from './components/ServicePanel.vue'
 import PageSkeleton from './components/PageSkeleton.vue';
-import type { SkuPopupLocaldata } from '@/components/vk-data-goods-sku-popup/vk-data-goods-sku-popup';
+import type { SkuPopupInstanceType, SkuPopupLocaldata } from '@/components/vk-data-goods-sku-popup/vk-data-goods-sku-popup';
 
 // 获取屏幕边界到安全区域距离
 const { safeAreaInsets } = uni.getSystemInfoSync()
@@ -40,11 +40,11 @@ const getGoodsByIdData = async () => {
   }
 }
 
-const isFinish=ref(false)
+const isFinish = ref(false)
 
 onLoad(async () => {
   await getGoodsByIdData()
-  isFinish.value=true
+  isFinish.value = true
 })
 
 
@@ -97,11 +97,24 @@ const openSkuPopup = (val: SkuMode) => {
   // 修改按钮模式
   mode.value = val
 }
+
+// SKU组件实例
+const skuPopupRef = ref<SkuPopupInstanceType>()
+
+// 计算被选中的值
+const selectArrText = computed(() => {
+  return skuPopupRef.value?.selectArr?.join(' ').trim() || '请选择商品规格'
+})
 </script>
 
 <template>
   <!-- SKU弹窗组件 -->
-  <vk-data-goods-sku-popup v-model="isShowSku" :localdata="localdata" :mode="mode" add-cart-background-color="#FFA868" buy-now-background-color="#27BA9B"  />
+  <vk-data-goods-sku-popup v-model="isShowSku" :localdata="localdata" :mode="mode" add-cart-background-color="#FFA868"
+    buy-now-background-color="#27BA9B" ref="skuPopupRef" :actived-style="{
+      color: '#27BA9B',
+      borderColor: '#27BA9B',
+      backgroundColor: '#E9F8F5',
+    }" />
   <scroll-view scroll-y class="viewport" v-if="isFinish">
     <!-- 基本信息 -->
     <view class="goods">
@@ -133,7 +146,7 @@ const openSkuPopup = (val: SkuMode) => {
       <view class="action">
         <view @tap="openSkuPopup(SkuMode.Both)" class="item arrow">
           <text class="label">选择</text>
-          <view  class="text ellipsis">请选择商品规格</view>
+          <view class="text ellipsis">{{ selectArrText }} </view>
         </view>
         <view @tap="openPopup('address')" class="item arrow">
           <text class="label">送至</text>
@@ -183,7 +196,7 @@ const openSkuPopup = (val: SkuMode) => {
       </view>
     </view>
 
-    
+
   </scroll-view>
   <PageSkeleton v-else />
 
